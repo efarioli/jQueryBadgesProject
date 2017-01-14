8e48e9667df0f5793a0178608e0bf1a156e63510
zeke farioli <efarioli@gmail.com>
2017-01-14 11:27:02 +0000

    first commit

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,11 @@
-$(function() {
-
-  // your code will go here
+$(document).ready(function () {
+  //your code will go here
+  $ajax({
+    url: 'https://www.codeschool.com/users/3217488.json',
+    dataType: 'jsonp',
+    success: function (response) {
+      // handle response
+    },
+  });
 
 });
