7258a676ae2ad505877f53fe02bd7c264d479fff
zeke farioli <efarioli@gmail.com>
2017-01-15 11:55:28 +0000

    Solved Project

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,37 @@
-$(document).ready(function () {
+$(function () {
   //your code will go here
-  $ajax({
+  $.ajax({
     url: 'https://www.codeschool.com/users/3217488.json',
     dataType: 'jsonp',
     success: function (response) {
       // handle response
+      addCourses(response.courses.completed);
+      console.log(response);
     },
   });
 
+  function addCourses(courses) {
+    var $badges = $('#badges');
+    courses.forEach(function (course) {
+      var $course = $('<div />', {
+        'class': 'course',
+      }).appendTo($badges);
+
+      console.log(course);
+
+      $('<h3 />', {
+        text: course.title,
+      }).appendTo($course);
+      $('<img />', {
+        src: course.badge,
+      }).appendTo($course);
+      $('<a />', {
+        href: course.url,
+        'class': 'btn btn-primary',
+        text: 'See Course',
+        target: '_blank',
+      }).appendTo($course);
+    });
+  };
+  $('.container h1').append('<p>Ezequiel Farioli</p>');
 });
